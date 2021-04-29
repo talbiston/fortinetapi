3f2b8c70bf4d7a688a746a1c24d57a22ccc45b66
Todd Albiston <todd.albiston@gtt.net>
2021-04-28 19:09:47 -0500

    commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'fortipyapi',
+    packages = ['fortipyapi'],
+    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = 'Todd Albiston',
+    author_email = 'foxtrot711@gmail.com',
+    url = 'https://github.com/talbiston/fortinetapi',
+    download_url = 'download link you saved',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
